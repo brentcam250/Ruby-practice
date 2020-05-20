fb0d8515f318d875f787f816511090ff9659a49e
Brent Cameron <bjcameron@gmail.com>
2020-05-20 15:29:21 -0600

    solved the caesars ciper

---
+++ caesar/caesar.rb
@@ -0,0 +1,32 @@
+def caesar_cipher(input, shift)
+  num_shift = shift.to_i
+  chars = input.split('')
+  alphabet = []
+  #build the alphabet array.
+  for letter in "a".."z" do
+    alphabet.push(letter)
+  end
+
+  upper_alphabet = alphabet.map{ |char| char.upcase}
+
+  output = ''
+  chars.each { |c| 
+    index = alphabet.index(c)
+    upcase_index = upper_alphabet.index(c);
+    #if index doesnt return nil, aka the letter is in the array and not a special char like punctuation.
+    if index 
+      shifted_index = (index + shift) % 26
+      output += (alphabet[shifted_index])
+    elsif upcase_index
+      shifted_index = (upcase_index + shift) % 26
+      output += (upper_alphabet[shifted_index])
+    else 
+      #just push the same char
+      output += (c)
+    end
+  }
+  return output
+
+end
+
+puts caesar_cipher('What a string!', 5)
