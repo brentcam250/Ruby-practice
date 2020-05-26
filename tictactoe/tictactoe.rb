19c38c26443308deeb46b5d2cc2af438e0f73a2c
Brent Cameron <bjcameron@gmail.com>
2020-05-26 15:36:13 -0600

    setting up class structures for the game

---
+++ tictactoe/tictactoe.rb
@@ -0,0 +1,19 @@
+class TicTacToe 
+  def display_board()
+    #this method to be used to show the current board.
+
+  end
+
+  def player_move(player_letter, space)
+    #this method to be called when a player makes a move
+  end
+
+  def win?() 
+    #this method to determine if a win has occured on the board.
+  end
+
+  def full_board
+    #this method to determine if all spaces have been played, aka a draw.
+  end
+
+end
