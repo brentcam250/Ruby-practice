39f392f28a9a9410b01a448f97dde87449b7782a
Brent Cameron <bjcameron@gmail.com>
2020-06-02 12:49:41 -0600

    made tictactoe playable, and declare winners and winning combo

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -25,11 +25,22 @@ class TicTacToe
 
   def player_move(player_letter, space)
     #this method to be called when a player makes a move
+
+
     unless(@board[space].is_full)
       @board[space].letter = player_letter
       @board[space].is_full = true
     else
-      puts "invalid space already has a letter"
+      puts "invalid space, choose an empty space"
+    end
+
+    if win?
+      #most recent player must have generated a win, then we have to end the game. 
+      puts "#{player_letter} Wins!"
+      #want to return so that no more letters can be played on a board thats already got a winner.
+      return
+    elsif full_board?
+      puts "Draw!"
     end
   end
 
@@ -37,7 +48,8 @@ class TicTacToe
     #this method to determine if a win has occured on the board.
     @@WIN_COMBOS.each { |combo|
       #print " 1st #{@board[combo[0]].letter} 2nd #{@board[combo[1]].letter} 3rd #{@board[combo[2]].letter} \n"
-      if((@board[combo[0]].letter == @board[combo[1]].letter)  && (@board[combo[1]].letter == @board[combo[2]].letter))
+      if((@board[combo[0]].letter == @board[combo[1]].letter)  && (@board[combo[1]].letter == @board[combo[2]].letter) && (@board[combo[1]].letter != ' '))
+        print " winning combo is: #{combo[0]}, #{combo[1]}, #{combo[2]} \n"
         return true
       end
     }
@@ -73,18 +85,19 @@ test_game = TicTacToe.new()
 
 test_game.display_board
 
-test_game.player_move("X", 3)
-test_game.player_move("X", 4)
-test_game.player_move("X", 5)
-test_game.player_move("X", 6)
-test_game.player_move("X", 7)
-test_game.player_move("X", 8)
-test_game.player_move("X", 0)
-test_game.player_move("X", 1)
-test_game.player_move("X", 2)
 
-test_game.display_board
-
-puts test_game.win?
-
-puts test_game.full_board?
+#x plays first, then we swap letters until winner or draw.
+x_turn = true
+until (test_game.win? || test_game.full_board?)
+  puts x_turn ? "Player X please type which square you'd like to play" : "Player O please type which square you'd like to play"
+  square = gets.chomp.to_i
+  until(square.between?(0,8))
+    puts "please play nicely, choose between 0, 8"
+    square = gets.chomp.to_i
+  end
+  current_letter = x_turn ? "X" : "O"
+  test_game.player_move(current_letter, square)
+  test_game.display_board
+  x_turn = !x_turn
+end
+  
