8a52c36d379595911b189a7cca39521f6abce8fd
Brent Cameron <bjcameron@gmail.com>
2020-06-04 09:33:23 -0600

    adding some more game functionality, made loop for guessing, built function to take input for a guess

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,38 +1,63 @@
 class Mastermind
-  attr_reader :secret_code, :number_guesses
+  attr_reader :secret_code, :total_guesses, :used_guesses
   def initialize
     @secret_code = set_secret_code
-    @number_guesses = set_guesses
+    @total_guesses = set_guesses
+    @used_guesses = 0
+    @correct_guess = false
   end
 
   def new_guess
     #this will take input from code-breaker and test to see if its right, or right places/numbers
+    puts "Codebreaker, please enter your guess code, I will tell you two things: the count of digits in the correct place,
+     and the count of correct digits in incorrect places"
+    guess = gets.chomp.to_i
+    puts guess
   end
 
-  def correct_number_and_place
+  def correct_number_and_place(guess)
     #count how many correct number and place compared to secret code
+    puts "correct num and place"
   end
 
-  def correct_number_only
+  def correct_number_only(guess)
     #count how many correct numbers but incorrect place.
+    puts "correct_number_only"
   end
 
   def play_game
-
+    until(@used_guesses >= @total_guesses || @correct_guess)
+      guess = new_guess
+      num_and_place = correct_number_and_place(guess)
+      just_num = correct_number_only(guess)
+      puts "Codebreaker: your guess of #{guess} has #{num_and_place} digits in the correct place and #{just_num} 
+      correct digits in the wrong place"
+    end
+    if(correct_guess)
+      "Congrats Codebreaker you win, the secret code was #{guess}!"
+    else 
+      "Sorry Codebreaker, Codemaker beat you this time"
+    end
   end
 
   def set_secret_code
     #takes input from codemaker player, and uses that to test codebreakers skills
     puts "Codemaker please enter a 4-digit number to be used as your secret code. make sure that the digits are between 1, 6"
-    secret_code = gets.chomp.to_i
-    until
+    secret_code = gets.chomp
+    unless secret_code.to_s.chars.map(&:to_i).each { |digit|
+      unless digit.between?(1,6)
+        puts "#{digit} not between 1,6 "
+      end
+    }
+    return secret_code
+    end
   end
 
   def set_guesses
     #takes input from codemaker player, uses that to set how many guesses codebreaker gets
     puts "Codemaker please enter a number between 8-12 for how many guesses you'd like codebreaker to have"
     guesses = gets.chomp.to_i
-    until(square.between?(8,12))
+    until(guesses.between?(8,12))
       puts "please play nicely, choose between 8, 12"
       guesses = gets.chomp.to_i
     end
@@ -41,3 +66,7 @@ class Mastermind
 
 
 end
+
+instance = Mastermind.new()
+
+instance.play_game
