9c31eb7de4805d23b2648c8a7031fc0738fd3188
Brent Cameron <bjcameron@gmail.com>
2020-06-09 10:29:31 -0600

    printing out first names from the files

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,14 @@
 puts "EventManager Initialized"
+
+# contents = File.read "../event_attendees.csv"
+# puts contents
+
+lines = File.readlines "../event_attendees.csv"
+row_index = 0
+lines.each do |line|
+  row_index = row_index + 1
+  next if row_index == 1
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
