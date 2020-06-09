2f3029c935c19662c8212ba89a94481d7f31c20e
Brent Cameron <bjcameron@gmail.com>
2020-06-09 10:44:42 -0600

    fixed bug where file string pointed to the wrong place

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,7 +1,7 @@
 require "csv"
 puts "EventManager Initialized"
 
-contents = CSV.open "event_attendees.csv", headers: true
+contents = CSV.open "../event_attendees.csv", headers: true
 contents.each do |row|
   name = row[2]
   puts name
