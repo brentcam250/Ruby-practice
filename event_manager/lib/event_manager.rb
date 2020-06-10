f6e2420cd98dfb653c75e091156ef6bfbe53e6d5
Brent Cameron <bjcameron@gmail.com>
2020-06-10 15:46:48 -0600

    using ERB to create some template letters with the info pulled from google

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,5 +1,6 @@
 require "csv"
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -16,28 +17,33 @@ def legislators_by_zipcode(zipcode)
     legislators = civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-
-    legislator_string = legislator_names.join(", ")
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
   rescue
     "You can find your represantatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end 
 
 end
+
+
 puts "Event Manager Intialized"
 
 
 contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
 
+template_letter = File.read "../form_letter.erb"
+erb_template = ERB.new template_letter
+
+
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-    puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  puts form_letter
+
 end
 
