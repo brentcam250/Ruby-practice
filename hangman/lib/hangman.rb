08d3a6864f881c0fe8fa9c30582dd9f949e464f2
Brent Cameron <bjcameron@gmail.com>
2020-06-11 13:40:52 -0600

    setting up skeleton of hangman Class

---
+++ hangman/lib/hangman.rb
@@ -0,0 +1,21 @@
+Class Hangman
+
+  def initialize
+    @secret_word = set_secret_word
+
+  end
+
+
+  def set_secret_word(length)
+
+  end
+
+  def new_guess(letter)
+
+  end
+
+  def display_letters_and_blanks
+
+  end
+
+end
