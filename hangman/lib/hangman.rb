7af07a7baaaec5a1c1b55149aa4ef46800bb4b3c
Brent Cameron <bjcameron@gmail.com>
2020-06-11 14:23:45 -0600

    able to randomly grab a word of appropriate length from the dict

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,20 +1,38 @@
-Class Hangman
+class Hangman
   attr_reader :secret_word
   attr_accessor :remaining_lives, :guessed_letters
 
-  def initialize(load?)
-    unless(load)
-      @secret_word = set_secret_word
-      @remaining_lives = 6
-      @guessed_letters = []
-    else
-      load_game
-    end
+  def initialize(load)
+    # unless(load)
+    #   @secret_word = set_secret_word
+    #   @remaining_lives = 6
+    #   @guessed_letters = []
+    # else
+    #   load_game
+    # end
+
+    @load = load
+    @secret_word = set_secret_word
 
   end
 
-  def set_secret_word(length)
-    dict = File.open('../dictionary.txt', 'r');
+  def find_random_word
+    #method to grab a random word from the dictionary, it doesnt check word length at all
+    File.open('../dictionary.txt', 'r') do |file|
+      file_lines = file.readlines()
+      random_line = file_lines[Random.rand(0..file_lines.size())]
+    end
+    
+  end
+
+  def set_secret_word
+    word = ''
+    until ((5..12).include?(word.length))
+      #until the random word is between our length requirements.
+      word = find_random_word
+      puts "word: #{word} length: #{word.length}"
+    end
+    return word
   end
 
   def new_guess
@@ -28,24 +46,26 @@ Class Hangman
 
   end
 
-  def display_letters_and_blanks
-    #this method is called after new guess is made, it will show the word as blanks with the correct guessed letters filled in
+  # def display_letters_and_blanks
+  #   #this method is called after new guess is made, it will show the word as blanks with the correct guessed letters filled in
 
-  end
+  # end
 
-  def save_game
+  # def save_game
 
-  end
+  # end
 
-  def load_game
+  # def load_game
 
-  end
+  # end
 
-  def play_game
-    while(@remaining_lives > 0)
+  # def play_game
+  #   while(@remaining_lives > 0)
 
-      @remaining_lives -= 1 
-    end
-  end
-
+  #     @remaining_lives -= 1 
+  #   end
+  # end
 end
+
+
+hangman = Hangman.new(false)
