0ad88befe0b6eb1e555829fc99bef413bd4b2d2c
Brent Cameron <bjcameron@gmail.com>
2020-06-12 13:34:14 -0600

    added some more game features, taking guesses, outputting current game state

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -2,17 +2,31 @@ class Hangman
   attr_reader :secret_word
   attr_accessor :remaining_lives, :guessed_letters
 
-  def initialize(load)
-    # unless(load)
-    #   @secret_word = set_secret_word
-    #   @remaining_lives = 6
-    #   @guessed_letters = []
-    # else
-    #   load_game
-    # end
+  def initialize
+    puts "WordSetter: do you wish you load a game? (y/n)"
+    response = gets.chomp.to_s.downcase
+    until(response == 'y' || response == 'n')
+      puts "Please respond either y or n"
+      response = gets.chomp.to_s.downcase
+    end
+    if(response == 'y')
+      load = true
+    else
+      load = false
+    end
+    unless(load)
+      @secret_word = set_secret_word
+      @remaining_lives = 6
+      @guessed_letters = []
+      @word_state = []
+      @word_state.fill('_', 0, secret_word.length)
+    else
+      load_game
+    end
 
-    @load = load
-    @secret_word = set_secret_word
+    play_game
+    # @load = load
+    # @secret_word = set_secret_word
 
   end
 
@@ -37,35 +51,54 @@ class Hangman
 
   def new_guess
     #takes user input for a new guess, display the board, if a win, end game
-    puts "Guesser: please choose a letter"
+    puts "\nGuesser: please choose a letter"
     guess = gets.chomp.to_s.downcase
-    while(guess.length != 1 || (a..z).include?(guess))
+    while(guess.length != 1 || !('a'..'z').include?(guess))
       puts "Guesser: please only choose a single letter"
       guess = gets.chomp.to_s.downcase
+    end
+    #else theyve played nicely and only guessed a single letter
+    if(@guessed_letters.include?(guess))
+      puts "you've already guessed that letter!"
+    else
+      # @remaining_lives -= 1 unless(@secret_word.include?(guess))
+      if(@secret_word.include?(guess))
+
+      else
+        @remaining_lives -= 1
+      end
+      @guessed_letters = @guessed_letters.push(guess)
     end
 
   end
 
-  # def display_letters_and_blanks
-  #   #this method is called after new guess is made, it will show the word as blanks with the correct guessed letters filled in
+  def display_letters_and_blanks
+    #this method is called after new guess is made, it will show the word as blanks with the correct guessed letters filled in
+    puts "Letters guessed: #{@guessed_letters}"
+    puts "Remaining lives: #{@remaining_lives}"
+    puts "State of your guessing: #{@word_state}"
 
-  # end
+  end
 
-  # def save_game
+  def save_game
+    #serialize the state of the game, and save that data into file
+  end
 
-  # end
+  def load_game
+    #get input from user which file contains their save file, and load that into the current session.
+    #deserialize data from file.
+    puts "Please type the name of your save file"
+    save_file = gets.chomp.to_s
+  end
 
-  # def load_game
-
-  # end
-
-  # def play_game
-  #   while(@remaining_lives > 0)
-
-  #     @remaining_lives -= 1 
-  #   end
-  # end
+  def play_game
+    while(@remaining_lives > 0)
+      new_guess
+      display_letters_and_blanks
+    end
+  end
 end
 
 
-hangman = Hangman.new(false)
+hangman = Hangman.new
+
