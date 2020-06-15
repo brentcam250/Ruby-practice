69f4c56ee31d604a3d61a00ed038e87bd6ec8ba7
Brent Cameron <bjcameron@gmail.com>
2020-06-15 15:18:42 -0600

    building merging function, not working yet bug seems to be with the recursive function trying to pass nil values

--- recursion/mergesort.rb
+++ recursion/mergesort.rb
@@ -1,23 +1,44 @@
 def merge_sort (array)
+    if !array 
+        puts "no array"
+    end
     if array.length > 2
         a = merge_sort(array.slice(0, array.length/2))
         b = merge_sort(array.slice(array.length/2, array.length-1))
-        return [a,b]
+        if(a && b)
+            return merge(a,b)
+        else 
+            puts "trying to pass nil arrays... "
+            return 0
+        end
     else 
         if array.length == 1 
             return array
         else
             if array[0] > array[1]
-                return [array[1], array[0]]
+                output = [array[1], array[0]]
+                puts "outputtting: #{output}"
+                return output
             else
-                # return [array[0], array[1]]
-                return array
+                return [array[0], array[1]]
+                # return array
             end
         end
     end
 
 end
 
+def merge (arr1, arr2)
+    total_length = arr1.length + arr2.length
+    output_array = []
+    while(arr1[0] && arr2[0])
+        if(arr1[0] > arr2[0])
+            output_array << arr2.shift
+        else 
+            output_array << arr1.shift
+        end
+    end
+end
 
 test_array = [10, 5, 3 , 6, 34, 1, 7 ]
 test_array_length_1 = [1]
