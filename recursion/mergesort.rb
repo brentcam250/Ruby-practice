f0bc5b076c1124014356983e1c37e055edca99e5
Brent Cameron <bjcameron@gmail.com>
2020-06-16 15:29:19 -0600

    fixed the bug with passing nil arrays, but still not merging it all back together properly

--- recursion/mergesort.rb
+++ recursion/mergesort.rb
@@ -1,29 +1,33 @@
 def merge_sort (array)
-    if !array 
+    puts "in the beginning there was #{array}"
+    if !array
         puts "no array"
     end
     if array.length > 2
-        a = merge_sort(array.slice(0, array.length/2))
+        a = merge_sort(array.slice(0, (array.length/2)))
         b = merge_sort(array.slice(array.length/2, array.length-1))
-        if(a && b)
-            return merge(a,b)
+        return merge(a,b) unless(a.nil? || b.nil?)
+        # if(a && b)
+        #     return merge(a,b)
+        # else 
+        #     puts "trying to pass nil arrays... "
+        #     return 0
+        # end
+    elsif array.length == 2 
+        if array[0] > array[1]
+            reverse = [array[1], array[0]]
+            puts "returning #{reverse} reverse order from the length 2 branch"
+            return reverse
         else 
-            puts "trying to pass nil arrays... "
-            return 0
-        end
-    else 
-        if array.length == 1 
+            puts "returning #{array} from length 2 branch"
             return array
-        else
-            if array[0] > array[1]
-                output = [array[1], array[0]]
-                puts "outputtting: #{output}"
-                return output
-            else
-                return [array[0], array[1]]
-                # return array
-            end
         end
+    elsif array.length == 1
+        puts "returning #{array} from the length 1 branch"
+        return array
+        
+    else 
+        puts "returning array of length #{array.length}"
     end
 
 end
@@ -38,6 +42,7 @@ def merge (arr1, arr2)
             output_array << arr1.shift
         end
     end
+    return output_array
 end
 
 test_array = [10, 5, 3 , 6, 34, 1, 7 ]
