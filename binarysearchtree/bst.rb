fd98841582742ac2a53ecbb9056dbfc88ce55199
Brent Cameron <bjcameron@gmail.com>
2020-06-22 10:27:41 -0600

    built node class, gave it a nice to_s replacement. working on the build_tree/insert node methods for that class

--- binarysearchtree/bst.rb
+++ binarysearchtree/bst.rb
@@ -4,15 +4,53 @@ class BinarySearchTree
 
   def initialize(array)
     @root = build_tree(array) 
-
+    @has_root = false
   end
 
   def build_tree(array)
+    #takes in array of values, returns the root node of the binary search tree.
+    array = array.sort
+    array = array.uniq
+    #sort and remove duplicates from the array.
+    
+    prev_node = nil
+    while(array.length > 0)
+      data = array.shift
+      
+      new_node = insert(data)
+    end
+  end
 
+  def root 
+    @root.data
   end
 
   def insert(value)
-
+    #build a new node and insert it in the correct spot in the tree.
+    new_node = BSTNode.new(value)
+    unless @has_root
+      @root = new_node
+      @has_root = true
+    end
+    current_node = @root
+    #start at the root and continue looking for the place to put the node.
+    while(current_node.left_child && current_node.right_child)
+      puts "comparing current: #{current_node.data} with new_node #{new_node.data}"
+      if(new_node.data < current_node.data)
+        current_node = current_node.left_child
+      elsif(new_node.data > current_node.data)
+        current_node = current_node.right_child
+      else  
+        puts "Something wrong with inserting, maybe a duplicate value?"
+      end
+    end
+    #now we come to a leaf
+    if(value < current_node.data)
+      current_node.left_child = new_node
+    else
+      current_node.right_child = new_node
+    end
+    return true
   end
 
   def delete(value)
@@ -55,9 +93,24 @@ end
 
 class BSTNode
   attr_accessor :data, :left_child, :right_child
-  def initialize(data = nil)
-    @data = data,
-    @left_child = nil
-    @right_child = nil
+  def initialize(data = nil, left_child = nil, right_child = nil)
+    @data = data 
+    @left_child = left_child
+    @right_child = right_child
   end
+
+  def to_s 
+    output = "NODE: data:#{@data ? @data : "nil"} left_child: #{@left_child ? @left_child : "nil"} right_child: #{@right_child ? @right_child : "nil"}"
+  end
+
 end
+
+test_array = [1,2,3]
+
+test_tree = BinarySearchTree.new(test_array)
+
+puts test_tree.root
+
+# test_node = BSTNode.new(5)
+
+# puts test_node
