b06ac715363ef77c34d4e215828171a8f1e271d0
Brent Cameron <bjcameron@gmail.com>
2020-06-22 11:18:43 -0600

    successfuly able to build tree from array, also implemented the find method

--- binarysearchtree/bst.rb
+++ binarysearchtree/bst.rb
@@ -1,6 +1,7 @@
 
 
 class BinarySearchTree
+  attr_accessor :root, :has_root
 
   def initialize(array)
     @root = build_tree(array) 
@@ -9,56 +10,95 @@ class BinarySearchTree
 
   def build_tree(array)
     #takes in array of values, returns the root node of the binary search tree.
-    array = array.sort
+    # array = array.sort
     array = array.uniq
-    #sort and remove duplicates from the array.
-    
-    prev_node = nil
+    #sort and remove duplicates from the array. 
+
+    #insert the root so we have a node to return as the instance variable @root   
+    root_value = array.shift
+    root_node = insert(nil, root_value)
+
+    #for the rest of the values in array, call the insert method
     while(array.length > 0)
-      data = array.shift
-      
-      new_node = insert(data)
+      new_value = array.shift
+      new_node = insert(root_node, new_value)
     end
+
+    return root_node
+
   end
 
-  def root 
-    @root.data
-  end
+  # def root 
+  #   @root
+  # end
 
-  def insert(value)
-    #build a new node and insert it in the correct spot in the tree.
-    new_node = BSTNode.new(value)
-    unless @has_root
-      @root = new_node
+
+  def insert(starting_node, value)
+    #recursive insert method
+    
+    unless(@has_root)
       @has_root = true
+      new_node = BSTNode.new(value)
+      @root = new_node
+      return new_node
     end
-    current_node = @root
-    #start at the root and continue looking for the place to put the node.
-    while(current_node.left_child && current_node.right_child)
-      puts "comparing current: #{current_node.data} with new_node #{new_node.data}"
-      if(new_node.data < current_node.data)
-        current_node = current_node.left_child
-      elsif(new_node.data > current_node.data)
-        current_node = current_node.right_child
-      else  
-        puts "Something wrong with inserting, maybe a duplicate value?"
+
+    if(value < starting_node.data)
+      if(starting_node.left_child)
+        #call insert again on the left subtree
+        insert(starting_node.left_child, value)
+      else
+        #no node here, so we have found the spot for our new node
+        new_node = BSTNode.new(value)
+        starting_node.left_child = new_node
+        return new_node
       end
+
+    elsif(value > starting_node.data)
+      if(starting_node.right_child)
+        insert(starting_node.right_child, value)
+      else
+        new_node = BSTNode.new(value)
+        starting_node.right_child = new_node
+        return new_node
+      end
+    else 
+      puts "equal values or some other error when inserting nodes"
     end
-    #now we come to a leaf
-    if(value < current_node.data)
-      current_node.left_child = new_node
-    else
-      current_node.right_child = new_node
-    end
-    return true
+    return false
+
   end
 
   def delete(value)
 
   end
 
-  def find(value)
-    #find value, return node with the value, or nil if not found
+  def find(starting_node = @root, value)
+    #find value, return node with the value, or false if not found
+    #use recursion to search through sub trees
+    
+    if(value == starting_node.data)
+
+      return starting_node
+
+    elsif(value < starting_node.data)
+
+      unless(starting_node.left_child.nil?)
+        find(starting_node.left_child, value)
+      else
+        return false
+      end
+
+    else
+
+      unless(starting_node.right_child.nil?)
+        find(starting_node.right_child, value)
+      else
+        return false
+      end
+      
+    end
+
   end
 
   def level_order(block)
@@ -100,16 +140,19 @@ class BSTNode
   end
 
   def to_s 
-    output = "NODE: data:#{@data ? @data : "nil"} left_child: #{@left_child ? @left_child : "nil"} right_child: #{@right_child ? @right_child : "nil"}"
+    output = "(NODE: data:#{@data ? @data : "nil"} left_child: #{@left_child ? @left_child : "nil"} right_child: #{@right_child ? @right_child : "nil"})"
   end
 
 end
 
-test_array = [1,2,3]
+test_array = [1,3,4,2]
 
 test_tree = BinarySearchTree.new(test_array)
 
 puts test_tree.root
+
+puts "find #{test_tree.find(8)}"
+
 
 # test_node = BSTNode.new(5)
 
