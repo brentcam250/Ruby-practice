b52431e4b8e9c3e8d1de0fd5de760291e954fec6
Brent Cameron <bjcameron@gmail.com>
2020-06-23 11:03:14 -0600

    added some more methods to the node class, implementings these classes in the delete method of the BST class

--- binarysearchtree/bst.rb
+++ binarysearchtree/bst.rb
@@ -65,26 +65,93 @@ class BinarySearchTree
   end
 
   def delete(value)
+    #returns true if deleted properly, false otherwise
     target_node = find_with_parent(value)
-    if target_node
+    target_on_right = true
+    if(target_node)
       to_be_deleted = target_node[1]
       parent_of_to_be_deleted = target_node[0]
-      if(to_be_deleted.left_child.nil? && to_be_deleted.right_child.nil?)
-        #no children so we can delete this worthless node
-        if(parent_of_to_be_deleted.left_child.data == value)
-          #we have to make sure that we delete the appropriate child node if there are more than one
-          parent_of_to_be_deleted.left_child = nil
-        else
-          parent_of_to_be_deleted.right_child = nil 
-        end
-      else
-        puts "shuffle children about"
+
+      if(parent_of_to_be_deleted.left_child.data == value)
+        #tobedeleted is on the left
+        target_on_right = false
       end
+
+      #section for dealing with cases of if the node we want to delete has children or not.
+      if(to_be_deleted.is_leaf?)
+        #no children, can just delete this straight off
+        if(target_on_right)
+          parent_of_to_be_deleted.right_child = nil
+          return true
+        else
+          parent_of_to_be_deleted.left_child = nil
+          return true
+        end
+      elsif(to_be_deleted.find_children == 1)
+        #has only right child in this case we can move the one child into the space previously occupied by the to_be_deleted
+        target_on_right ? parent_of_to_be_deleted.right_child = to_be_deleted.right_child : parent_of_to_be_deleted.left_child = to_be_deleted.right_child
+        return true
+      elsif(to_be_deleted.find_children == -1)
+        #has only left child
+        target_on_right ? parent_of_to_be_deleted.right_child = to_be_deleted.left_child : parent_of_to_be_deleted.left_child = to_be_deleted.left_child
+        return true
+      else
+        #to_be_deleted has two children
+
+      end
+
     else
-      puts " that node doesnt exist bozo"
+      puts "delete: that value cannot be found in the tree"
       return false
     end
   end
+
+  # def delete(value)
+  #   target_node = find_with_parent(value)
+  #   target_on_right = true
+
+  #   if target_node
+  #     to_be_deleted = target_node[1]
+  #     parent_of_to_be_deleted = target_node[0]
+  #     puts "parent = #{parent_of_to_be_deleted}"
+  #     if(parent_of_to_be_deleted.left_child.data == value)
+  #       #to be deleted is onthe left
+  #       target_on_right = false
+  #     end
+  #     if(to_be_deleted.left_child.nil? && to_be_deleted.right_child.nil?)
+  #       #no children so we can delete this worthless node
+  #       if(target_on_right)
+  #         #we have to make sure that we delete the appropriate child node if there are more than one
+  #         parent_of_to_be_deleted.right_child = nil
+  #       else
+  #         parent_of_to_be_deleted.left_child = nil 
+  #       end
+  #     elsif (to_be_deleted.left_child.nil? || to_be_deleted.right_child.nil? )
+  #       #only one child, can just move the child to take the place of the deleted node
+  #       if(to_be_deleted.left_child.nil?)
+  #         #the child is on the right
+  #         if(target_on_right)
+  #           parent_of_to_be_deleted.right_child = to_be_deleted.right_child
+  #         else
+  #           parent_of_to_be_deleted.left_child = to_be_deleted.right_child
+  #         end
+  #       else
+  #         #child is on the left
+  #         if(target_on_right)
+  #           parent_of_to_be_deleted.right_child = to_be_deleted.left_child
+  #         else
+  #           parent_of_to_be_deleted.left_child = to_be_deleted.left_child
+  #         end
+          
+  #       end
+  #       puts "shuffle children about"
+  #       puts parent_of_to_be_deleted
+  #     end
+  #   else
+  #     puts " that node doesnt exist bozo"
+  #     return false
+  #   end
+  # end
 
   def find_with_parent(starting_node = @root, parent_node = nil, value)
       #find value, return array of nodes 
@@ -186,21 +253,44 @@ class BSTNode
     output = "(NODE: data:#{@data ? @data : "nil"} left_child: #{@left_child ? @left_child : "nil"} right_child: #{@right_child ? @right_child : "nil"})"
   end
 
+  def is_leaf?
+    if(left_child.nil? && right_child.nil?)
+      return true
+    else
+      return false
+    end
+  end
+
+  def find_children
+    #returns 1 if right only, -1 if left only 0 if two children, false if none
+    if(left_child.nil? && !right_child.nil?)
+      #only a right child
+      return 1
+    elsif (!left_child.nil? && right_child.nil?)
+      #only a left child
+      return -1
+    elsif(this.is_leaf?)
+      return false
+    else
+      return 0
+    end
+
+  end
+
 end
 
 test_array = [1,3,4,2]
 
 array =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-test_tree = BinarySearchTree.new(array)
+test_tree = BinarySearchTree.new(test_array)
 
 # puts test_tree.root
 
-puts test_tree.find_with_parent(6345)
 
-test_tree.delete(324)
+test_tree.delete(3)
 
-puts test_tree.find(324)
+puts test_tree.find(3)
 # puts "find #{test_tree.find(8)}"
 # puts "find #{test_tree.find(6345)}"
 
